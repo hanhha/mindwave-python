facf3a2e31f8f0fd9c38e5e2347da370c11b73b3
JoBergs <jo@knight-of-pi.org>
2016-06-04 16:41:07 +0200

    added test script for RPi gpio interface

---
+++ mindwave_LED.py
@@ -0,0 +1,35 @@
+import mindwave, time
+import RPi.GPIO as GPIO
+
+# initialise PWM pin
+GPIO.setmode(GPIO.BOARD)  # BOARD numbering
+
+LED = 11  # BOARD Pin 11 == BCM Pin 17
+GPIO.setup(LED, GPIO.OUT)
+
+pwm_LED = GPIO.PWM(LED, 100)
+pwm_LED.start(0)
+
+# setup headset
+headset = mindwave.Headset('/dev/ttyUSB3', '1E5F')
+time.sleep(2)
+
+headset.connect()
+print "Connecting..."
+
+while headset.status != 'connected':
+    time.sleep(0.5)
+    if headset.status == 'standby':
+        headset.connect()
+        print "Retrying connect..."
+print "Connected."
+
+while True:
+    time.sleep(.5)
+    print "Attention: %s, Meditation: %s" % (headset.attention, headset.meditation)
+    if headset.attention > 30:
+        pwm_LED.ChangeDutyCycle(min(100, headset.attention)) 
+
+
+
+
